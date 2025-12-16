563d9776e8a4a6c79caea803a7600550f577a471
itscitti4-cloud <itscitti4@gmail.com>
2025-12-17 02:00:29 +0600

    Update developer.js

--- scripts/cmds/developer.js
+++ scripts/cmds/developer.js
@@ -6,7 +6,7 @@ module.exports = {
                 name: "developer",
                 aliases: ["dev"],
                 version: "1.0",
-                author: "NeoKEX",
+                author: "AkHi",
                 countDown: 5,
                 role: 4,
                 description: {
