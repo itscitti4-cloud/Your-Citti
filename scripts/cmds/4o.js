426613d31eef44cb5c616d6de81c9331eb1af1da
itscitti4-cloud <itscitti4@gmail.com>
2025-12-17 14:40:15 +0600

    Update 4o.js

--- scripts/cmds/4o.js
+++ scripts/cmds/4o.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "4o",
     aliases: ["gpt4o", "dalle4o"],
     version: "1.0",
-    author: "Neoaz ゐ", //API by RIFAT
+    author: "AkHi",
     countDown: 10,
     role: 0,
     shortDescription: { en: "Generate AI image with 4o" },
