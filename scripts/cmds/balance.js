a6ac3263f4ac007e665b5f9fa57e7a103a176531
itscitti4-cloud <itscitti4@gmail.com>
2025-12-19 03:38:03 +0600

    Update balance.js

--- scripts/cmds/balance.js
+++ scripts/cmds/balance.js
@@ -311,7 +311,7 @@ module.exports = {
         name: "balancec",
         aliases: ["bal", "wallet", "mybalance", "wcard"],
         version: "2.0.0",
-        author: "Neoaz ゐ",
+        author: "AkHi",
         countDown: 10,
         role: 0,
         description: "Display your wallet balance with a professional card featuring your profile picture",
