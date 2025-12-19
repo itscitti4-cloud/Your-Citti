b204574a4083599dd3fec848556e9ffbc5850dd1
itscitti4-cloud <itscitti4@gmail.com>
2025-12-19 22:52:53 +0600

    Update boxinfo.js

--- scripts/cmds/boxinfo.js
+++ scripts/cmds/boxinfo.js
@@ -8,10 +8,10 @@ config: {
     version: "1.0",
     author: "AkHi",
     countDown: 5,
-    role: 0,
+    role: 2,
     shortDescription: "See Box info",
     longDescription: "",
-    category: "box chat",
+    category: "information",
     guide: {
       en: "{p} [groupinfo|boxinfo]",
     }
@@ -52,7 +52,7 @@ const name = infu[qtv2[i].id].name;
       var callback = () =>
         api.sendMessage(
           {
-            body: `🔧「 Group Name 」:${threadName}\n🔧「 Group id 」: ${id}\n🔧「 Approval 」: ${pd}\n🔧「 Emoji 」: ${icon}\n🔧「 Information 」: 𝐈𝐧𝐜𝐥𝐮𝐝𝐢𝐧𝐠 ${threadMem} 𝐌𝐞𝐦𝐛𝐞𝐫𝐬\n🔧「 Number of Males 」: ${nam}\n🔧「 Number of Females」:  ${nu}\n🔧「 Total Administrator 」: ${qtv} \n「 𝐈𝐧𝐜𝐥𝐮𝐝𝐞 」:\n${listad}\n🔧「 Total Number of Message 」: ${sl} msgs.\n\n Bot Admin and Developer: Lubna Jannat AkHi`,
+            body: `🔧「 𝐆𝐂 𝐍𝐚𝐦𝐞 」:${threadName}\n🔧「 𝐆𝐫𝐨𝐮𝐩 𝐈𝐃 」: ${id}\n🔧「 𝐀𝐩𝐩𝐫𝐨𝐯𝐚𝐥 」: ${pd}\n🔧「 𝐄𝐦𝐨𝐣𝐢 」: ${icon}\n🔧「 𝐈𝐧𝐟𝐨𝐫𝐦𝐚𝐭𝐢𝐨𝐧 」: 𝐈𝐧𝐜𝐥𝐮𝐝𝐢𝐧𝐠 ${threadMem} 𝐌𝐞𝐦𝐛𝐞𝐫𝐬\n🔧「 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐌𝐚𝐥𝐞𝐬 」: ${nam}\n🔧「 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐅𝐞𝐦𝐚𝐥𝐞𝐬 」:  ${nu}\n🔧「 𝐓𝐨𝐭𝐚𝐥 𝐀𝐝𝐦𝐢𝐧𝐢𝐬𝐭𝐫𝐚𝐭𝐨𝐫𝐬 」: ${qtv} \n「 𝐈𝐧𝐜𝐥𝐮𝐝𝐞 」:\n${listad}\n🔧「 𝐓𝐨𝐭𝐚𝐥 𝐍𝐮𝐦𝐛𝐞𝐫 𝐎𝐟 𝐌𝐞𝐬𝐬𝐚𝐠𝐞𝐬 」: ${sl} msgs.\n\nBot admin & developer: Lubna Jannat AkHi`,
             attachment: fs.createReadStream(__dirname + '/cache/1.png')
           },
           event.threadID,
