92974151de09add2898f104803afaf0a99e17f48
itscitti4-cloud <itscitti4@gmail.com>
2025-12-20 20:01:18 +0600

    Update post.js

--- scripts/cmds/post.js
+++ scripts/cmds/post.js
@@ -20,4 +20,12 @@ module.exports = {
     const { config } = global.GoatBot;
     const postText = args.join(" ");
 
-    if (!postText) return api.sendMessage("Wrong format! post <post text>", thread
+api.createPost(messageContent, (err, data) => {
+    if (err) {
+        return api.sendMessage("Post failed!", threadID);
+    }
+  if (!postText) return api.sendMessage("Wrong format! post <post text>", thread};
+    // পোস্ট সফল হলে এই মেসেজটি দিবে
+    api.sendMessage("Post done successfully Ma'am", threadID);
+});
+    
