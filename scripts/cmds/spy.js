98ade7cf4e4d8f4bb4cc1976f126d441b603c7c9
itscitti4-cloud <itscitti4@gmail.com>
2025-12-28 18:19:36 +0600

    Update spy.js

--- scripts/cmds/spy.js
+++ scripts/cmds/spy.js
@@ -1,4 +1,10 @@
 const axios = require("axios");
+const baseApiUrl = async () => {
+  const base = await axios.get(
+    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+  );
+  return base.data.api;
+};
 
 module.exports = {
   config: {
