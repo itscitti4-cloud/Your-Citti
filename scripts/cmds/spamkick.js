c01897bb26542ed5dfd16ccd806ecab2fdbfea20
itscitti4-cloud <itscitti4@gmail.com>
2026-01-10 23:36:29 +0600

    Update spamkick.js

--- scripts/cmds/spamkick.js
+++ scripts/cmds/spamkick.js
@@ -1,6 +1,6 @@
 module.exports.config = {
   name: "spamkick",
-  version: "1.0.0",
+  version: "1.0.1",
   role: 0, 
   author: "AkHi",
   usePrefix: true,
@@ -11,11 +11,15 @@ module.exports.config = {
   guide: { en:"[on/off] or [settings]"},
   countDown: 5
 };
+
 module.exports.onChat = async ({ api, event, usersData, commandName }) => {
   const { senderID, threadID } = event;
   if (!global.antispam) global.antispam = new Map();
 
-  const threadInfo = global.antispam.has(threadID) ? global.antispam.get(threadID) : { users: {} };
+  // চেক করা হচ্ছে এই থ্রেডে অ্যান্টি-স্প্যাম অন আছে কি না
+  if (!global.antispam.has(threadID)) return;
+
+  const threadInfo = global.antispam.get(threadID);
   if (!(senderID in threadInfo.users)) {
     threadInfo.users[senderID] = { count: 1, time: Date.now() };
   } else {
@@ -23,20 +27,32 @@ module.exports.onChat = async ({ api, event, usersData, commandName }) => {
     const timePassed = Date.now() - threadInfo.users[senderID].time;
     const messages = threadInfo.users[senderID].count;
     const timeLimit = 80000;
-    const messageLimit = 14; //Limit of message
+    const messageLimit = 14; 
 
     if (messages > messageLimit && timePassed < timeLimit) {
       if(global.GoatBot.config.adminBot.includes(senderID)) return;
+
+      // সরাসরি ফেসবুক থেকে ডাটা নিয়ে অ্যাডমিন চেক
+      const facebookThreadInfo = await api.getThreadInfo(threadID);
+      const botID = api.getCurrentUserID();
+      const adminIDs = facebookThreadInfo.adminIDs.map(admin => admin.id);
+
+      if (!adminIDs.includes(botID)) {
+        // বট অ্যাডমিন না হলে ডাটা রিসেট করে মেসেজ দিবে
+        threadInfo.users[senderID] = { count: 1, time: Date.now() };
+        return api.sendMessage("🚫 Spam detected! But I can't kick the user because I am not an admin.", threadID);
+      }
+
       api.removeUserFromGroup(senderID, threadID, async (err) => {
         if (err) {
           console.error(err);
         } else {
-          api.sendMessage({body: `${await usersData.getName(senderID)} has been removed for spamming.\nUser ID: ${senderID}\n React in this message to add him again.`}, threadID, (error,info) => {
+          api.sendMessage({body: `${await usersData.getName(senderID)} has been removed for spamming.\nUser ID: ${senderID}\nReact to this message to add him again.`}, threadID, (error,info) => {
               global.GoatBot.onReaction.set(info.messageID, { 
                   commandName, 
                   uid: senderID,
                   messageID: info.messageID
-        });
+              });
           });
         }
       });
@@ -48,51 +64,58 @@ module.exports.onChat = async ({ api, event, usersData, commandName }) => {
   }
 
   global.antispam.set(threadID, threadInfo);
-
 };
 
 module.exports.onReaction = async ({ api, event, Reaction, threadsData, usersData , role }) => {
     const { uid, messageID } = Reaction;
-    const { adminIDs, approvalMode } = await threadsData.get(event.threadID);
+    const { threadID } = event;
+    
+    // রিয়েল-টাইম ডাটা চেক
+    const facebookThreadInfo = await api.getThreadInfo(threadID);
+    const adminIDs = facebookThreadInfo.adminIDs.map(admin => admin.id);
+    const approvalMode = facebookThreadInfo.approvalMode;
     const botID = api.getCurrentUserID();
+
     if (role < 1) return;
     var msg = "";
 
-      try {
-          await api.addUserToGroup(uid, event.threadID);
-          if (approvalMode === true && !adminIDs.includes(botID)){
-              msg += `Successfully added ${await usersData.getName(uid)} to approval list.`;
-              await api.unsendMessage(messageID);
-          }
-          else{
-              msg += `Successfully added ${await usersData.getName(uid)} to this group.`;
-              await api.unsendMessage(messageID);
-          }
-      }
-      catch (err) {
-          msg += `Failed to add ${await usersData.getName(uid)} to this group.`;
-      }
-      console.log(msg);
-  }
+    try {
+        if (!adminIDs.includes(botID)) {
+            return api.sendMessage("I need to be an admin to add users back.", threadID);
+        }
 
-
+        await api.addUserToGroup(uid, threadID);
+        if (approvalMode === true && !adminIDs.includes(botID)){
+            msg += `Successfully added ${await usersData.getName(uid)} to approval list.`;
+            await api.unsendMessage(messageID);
+        }
+        else{
+            msg += `Successfully added ${await usersData.getName(uid)} to this group.`;
+            await api.unsendMessage(messageID);
+        }
+    }
+    catch (err) {
+        msg += `Failed to add ${await usersData.getName(uid)} to this group.`;
+    }
+    api.sendMessage(msg, threadID);
+};
 
 module.exports.onStart = async ({ api, event, args }) => {
   switch (args[0]) {
       case "on":
-if (!global.antispam) global.antispam = new Map();
+        if (!global.antispam) global.antispam = new Map();
         global.antispam.set(event.threadID, { users: {} });
-        api.sendMessage("Spam kick has been turned on for this Group.", event.threadID,event.messageID);
+        api.sendMessage("Spam kick has been turned on for this Group.", event.threadID, event.messageID);
         break;
       case "off":
         if (global.antispam && global.antispam.has(event.threadID)) {
           global.antispam.delete(event.threadID);
-          api.sendMessage("Spam kick has been turned off for this group", event.threadID,event.messageID);
+          api.sendMessage("Spam kick has been turned off for this group", event.threadID, event.messageID);
         } else {
-          api.sendMessage("Spam kick is not active on this group", event.threadID,event.messageID);
+          api.sendMessage("Spam kick is not active on this group", event.threadID, event.messageID);
         }
         break;
       default:
-        api.sendMessage("Please use 'on' to activate or 'off' to deactivate the Spam kick.", event.threadID,event.messageID);
+        api.sendMessage("Please use 'on' to activate or 'off' to deactivate the Spam kick.", event.threadID, event.messageID);
     }
-  };
+};
